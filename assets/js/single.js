b08747469892eff06e2d78be346276ca2096d7bc
deb boro <debajit.boro666@gmail.com>
2022-06-03 09:17:24 -0400

    updated single.js

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector('#issues-container')
+
 var displayIssues = function (issues) {
   for (var i = 0; i < issues.length; i++) {
     if (issues.length === 0) {
@@ -6,32 +7,38 @@ var displayIssues = function (issues) {
       return
     }
     // create a link element to take users to the issue on github
-    var issueEl = document.createElement('a')
-    issueEl.classList = 'list-item flex-row justify-space-between align-center'
-    issueEl.setAttribute('href', issues[i].html_url)
-    issueEl.setAttribute('target', '_blank')
-    var titleEl = document.createElement('span')
-    titleEl.textContent = issues[i].title
 
-    // append to container
-    issueEl.appendChild(titleEl)
+    for (var i = 0; i < issues.length; i++) {
+      var repoName = issues[i].user.login + ' / ' + issues[i].name
+      var issueEl = document.createElement('a')
+      issueEl.classList =
+        'list-item flex-row justify-space-between align-center'
+      issueEl.setAttribute('href', issues[i].html_url)
+      issueEl.setAttribute('target', '_blank')
 
-    // create a type element
-    var typeEl = document.createElement('span')
+      //create span to hold issue title
+      var titleEl = document.createElement('span')
+      titleEl.textContent = issues[i].title
 
-    // check if issue is an actual issue or a pull request
-    if (issues[i].pull_request) {
-      typeEl.textContent = '(Pull request)'
-    } else {
-      typeEl.textContent = '(Issue)'
+      // append to container
+      issueEl.appendChild(titleEl)
+
+      // create a type element
+      var typeEl = document.createElement('span')
+
+      // check if issue is an actual issue or a pull request
+      if (issues[i].pull_request) {
+        typeEl.textContent = '(Pull request)'
+      } else {
+        typeEl.textContent = '(Issue)'
+      }
+
+      // append to container
+      issueEl.appendChild(typeEl)
+      issueContainerEl.appendChild(issueEl)
     }
-
-    // append to container
-    issueEl.appendChild(typeEl)
-    issueContainerEl.appendChild(issueEl)
   }
 }
-
 var getRepoIssues = function (repo) {
   var apiUrl = 'https://api.github.com/repos/' + repo + '/issues?direction=asc'
 
@@ -39,9 +46,9 @@ var getRepoIssues = function (repo) {
     if (Response.ok) {
       Response.json().then(function (data) {
         displayIssues(data)
-        if(Response.headers.get("Link"){
-            console.log("repo has more than 30 issues")
-        })
+        if (Response.headers.get('Link')) {
+          console.log('repo has more than 30 issues')
+        }
       })
     } else {
       alert('there was a problem with your request!')
